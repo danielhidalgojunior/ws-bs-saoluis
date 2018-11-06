68d86aae6ff467dd094f3ed0ce90229a948798db
Daniel SL <daniel.hidalgo@etec.sp.gov.br>
2018-11-06 21:58:31 -0200

    adicionado modal para CRUD

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,9 +11,9 @@ function loadDummies() {
             '<img class="list-image" src="' + x.img + '" alt="">' +
             '<span>' + x.name + '</span>' +
             '<div class="list-item-buttons-div">' +
-            '<a class="btn btn-info" onclick="editProduct(' + x.id + ')">Editar</a>' +
+            '<a class="btn btn-info" style="color:white" data-toggle="modal" data-target="#myModal" onclick="editProduct(' + x.id + ')">Editar</a>' +
             '<br><br>' +
-            '<a class="btn btn-danger" href="#">Remover</a>' +
+            '<a class="btn btn-danger" style="color:white" onclick="removeProduct(' + x.id + ')">Remover</a>' +
             '</div>' +
             '</div>' +
             '</div>' +
@@ -23,8 +23,16 @@ function loadDummies() {
 }
 
 function editProduct(id) {
-    console.log("passei aqui", id);
- }
+    console.log("edit", id);
+}
+
+function removeProduct(id) {
+    console.log("remove", id);
+}
+
+function insertProduct(id) {
+    console.log("insert", id);
+}
 
 var products = [
     {
@@ -38,5 +46,11 @@ var products = [
         name: "Ryzen 5 2600x",
         img: "https://images3.kabum.com.br/produtos/fotos/95563/95563_1522844766_index_gg.jpg",
         units: 10
+    },
+    {
+        id: 2,
+        name: "Xeon 34 3H4U3H583HF87YF",
+        img: "https://http2.mlstatic.com/processador-servidor-intel-e3-1220v6-xeon-1151-300-ghz-box-D_NQ_NP_776679-MLB27721776330_072018-F.webp",
+        units: 5
     }
 ];
